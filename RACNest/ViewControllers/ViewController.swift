5590db6178e14e1c6bf79d1cef407f20c2b6749c
Rui Peres <rui.peres@mailonline.co.uk>
2016-01-13 23:09:03 +0000

    Initial Project structure

---
+++ RACNest/ViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RACNest
+//
+//  Created by Rui Peres on 13/01/2016.
+//  Copyright © 2016 Rui Peres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
