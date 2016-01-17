49f328b123d008fca83c5aecf7ddc04d97582771
Rui Peres <rui.peres@mailonline.co.uk>
2016-01-17 01:37:05 +0000

    I should send a PR to Neilpa/Rex with this

---
+++ RACNest/Components/RAC Extensions/RacExtensions.swift
@@ -0,0 +1,24 @@
+//
+//  RacExtensions.swift
+//  RACNest
+//
+//  Created by Rui Peres on 17/01/2016.
+//  Copyright © 2016 Rui Peres. All rights reserved.
+//
+
+import UIKit
+import ReactiveCocoa
+import Rex
+
+extension UITextField {
+
+    public var rex_textSignal: SignalProducer<String, NoError> {
+        
+        return NSNotificationCenter.defaultCenter()
+            .rac_notifications(UITextFieldTextDidChangeNotification, object: self)
+            .filterMap { notification in
+                guard let textField = notification.object as? UITextField else { return nil }
+                return textField.text ?? ""
+        }
+    }
+}
