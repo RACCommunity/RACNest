f4aeb991808b95afb08e14433b15f3717c845930
Rui Peres <rui.peres@mailonline.co.uk>
2016-01-20 23:13:14 +0000

    Initial work on the search

---
+++ RACNest/ViewControllers/Search/SearchViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SearchViewController.swift
+//  RACNest
+//
+//  Created by Rui Peres on 20/01/2016.
+//  Copyright © 2016 Rui Peres. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+}
+
