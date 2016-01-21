098984b8ab6ed4a21ec92feaf001a1c45769f99b
Rui Peres <rui.peres@mailonline.co.uk>
2016-01-21 01:54:26 +0000

    First draft.
    So the idea on line 36 and 36 is: I want to change the state to loading, when I am about to start a new filter. So the UI can be updated.

--- RACNest/ViewControllers/Search/SearchViewModel.swift
+++ RACNest/ViewControllers/Search/SearchViewModel.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import ReactiveCocoa
+import Rex
 
 enum SearchStatus<T> {
     case Loading
@@ -23,10 +24,30 @@ class SearchViewModel {
     
     init() {
         
-        dataSource <~ SearchViewModel.generateDataSource()
-            .startOn(QueueScheduler(name: "private_queue"))
+        let dataSourceGenerator = SearchViewModel.generateDataSource()
+            .startOn(QueueScheduler(name: "GenerateDataSource"))
         
-        result <~ SignalProducer(value: dataSource.value).map { SearchStatus.Valid($0) }
+        let dataSourceGeneratorTrigger: SignalProducer<Void, NoError> = dataSourceGenerator.map{_ in () }.ignoreError()
+        
+        dataSource <~ dataSourceGenerator
+
+        let producer = searchText.producer
+        
+        result <~ producer.map {_ in .Loading }
+        result <~ producer
+            .skipUntil(dataSourceGeneratorTrigger)
+            .throttle(0.3, onScheduler: QueueScheduler(name: "TextThrottle"))
+            .map(wordsSubSet)
+            .map { .Valid($0) }
+    }
+    
+    private func wordsSubSet(word: String) -> [String] {
+        
+        guard  word != "" else { return dataSource.value }
+        
+       return dataSource.value.filter {
+           $0.rangeOfString(word, options: .CaseInsensitiveSearch) !=  nil
+        }
     }
     
     static private func generateDataSource() -> SignalProducer<[String], NoError> {
