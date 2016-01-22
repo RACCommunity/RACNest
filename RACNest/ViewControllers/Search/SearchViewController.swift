aceec6b504553d68ff8fbdae299ea29ebfb259d2
Rui Peres <rui.peres@mailonline.co.uk>
2016-01-22 00:11:27 +0000

    Hook up Between VC and VM

--- RACNest/ViewControllers/Search/SearchViewController.swift
+++ RACNest/ViewControllers/Search/SearchViewController.swift
@@ -7,8 +7,51 @@
 //
 
 import UIKit
+import ReactiveCocoa
 
 class SearchViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+
+    private let viewModel = SearchViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.registerReusableCell(MainViewTableCell)
+                
+        viewModel.result.producer.observeOn(QueueScheduler.mainQueueScheduler).startWithNext {[weak self] text in
+            
+            print(text.count)
+            self?.tableView.reloadData()
+        }
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        viewModel.searchText.value = searchText
+    }
+}
+
+extension SearchViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.result.value.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell: MainViewTableCell = tableView.dequeueReusableCell(indexPath: indexPath)
+
+        let searchValue = viewModel.result.value[indexPath.row]
+        let searchCellItem = SearchCellItem(title: searchValue)
+        
+        cell.configure(searchCellItem)
+        
+        return cell
+    }
 }
 
